efefc5b232b61442bf12f95e17783acb5bf7248b
Pedro Augusto <pedroaugustomoura000@gmail.com>
2025-07-24 12:57:31 -0300

    Primeiro commit

---
+++ src/main/java/io/pedroaugusto00/kommanderstore/KommanderstoreApplication.java
@@ -0,0 +1,13 @@
+package io.pedroaugusto00.kommanderstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KommanderstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KommanderstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/pedroaugusto00/kommanderstore/KommanderstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pedroaugusto00.kommanderstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KommanderstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
