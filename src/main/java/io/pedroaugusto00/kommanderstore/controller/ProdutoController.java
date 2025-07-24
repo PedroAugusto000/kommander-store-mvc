5659a27d1e88b54c83fbffcc3319c9bb89002cf8
Pedro Augusto <pedroaugustomoura000@gmail.com>
2025-07-24 14:11:01 -0300

    Criado Create de produtos + Erro com UUID no banco postgres e a aplicação

--- src/main/java/io/pedroaugusto00/kommanderstore/controller/ProdutoController.java
+++ src/main/java/io/pedroaugusto00/kommanderstore/controller/ProdutoController.java
@@ -1,12 +1,32 @@
 package io.pedroaugusto00.kommanderstore.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.pedroaugusto00.kommanderstore.controller.dto.ProdutoDTO;
+import io.pedroaugusto00.kommanderstore.service.ProdutoService;
+
 @RestController
+@RequestMapping("/produtos")
 public class ProdutoController {
 	
+	private final ProdutoService produtoService;
 
+	public ProdutoController(ProdutoService produtoService) {
+		this.produtoService = produtoService;
+	}
+	
+	
+	@PostMapping
+	public ResponseEntity<ProdutoDTO> criarProduto(@RequestBody ProdutoDTO dto) {
+		ProdutoDTO salvo = produtoService.salvar(dto);
+		return new ResponseEntity<>(salvo, HttpStatus.CREATED);
+	}
 	
 }
--- src/main/java/io/pedroaugusto00/kommanderstore/service/ProdutoService.java
+++ src/main/java/io/pedroaugusto00/kommanderstore/service/ProdutoService.java
@@ -1,11 +1,27 @@
 package io.pedroaugusto00.kommanderstore.service;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Service;
+
+import io.pedroaugusto00.kommanderstore.controller.dto.ProdutoDTO;
+import io.pedroaugusto00.kommanderstore.controller.mapper.ProdutoMapper;
+import io.pedroaugusto00.kommanderstore.model.Produto;
+import io.pedroaugusto00.kommanderstore.repository.ProdutoRepository;
 
 @Service
 public class ProdutoService {
 
-	public void salvar() {
-		
+	private final ProdutoRepository produtoRepository;
+	
+	public ProdutoService(ProdutoRepository produtoRepository) {
+		this.produtoRepository = produtoRepository;
+	}
+
+	public ProdutoDTO salvar(ProdutoDTO dto) {
+		Produto produto = ProdutoMapper.toEntity(dto);
+		Produto salvo = produtoRepository.save(produto);
+		produto.setDataCriacao(LocalDateTime.now());
+		return ProdutoMapper.toDTO(salvo);
 	}
 }
