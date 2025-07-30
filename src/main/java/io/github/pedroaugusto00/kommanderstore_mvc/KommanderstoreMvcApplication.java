88abd8cd466727f5612e94d939949c847546343d
Pedro Augusto <pedroaugustomoura000@gmail.com>
2025-07-30 12:09:39 -0300

    Projeto puro

---
+++ src/main/java/io/github/pedroaugusto00/kommanderstore_mvc/KommanderstoreMvcApplication.java
@@ -0,0 +1,13 @@
+package io.github.pedroaugusto00.kommanderstore_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KommanderstoreMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KommanderstoreMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/pedroaugusto00/kommanderstore_mvc/KommanderstoreMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pedroaugusto00.kommanderstore_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KommanderstoreMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
